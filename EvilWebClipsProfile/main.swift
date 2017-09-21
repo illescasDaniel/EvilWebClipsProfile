1226b66c09ad9fafbdcacd6d0f043b1b4732dd31
illescasDaniel <illescas.daniel@protonmail.com>
2017-09-21 14:02:21 +0200

    Initial Commit

---
+++ EvilWebClipsProfile/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EvilWebClipsProfile
+//
+//  Created by Daniel Illescas Romero on 21/09/2017.
+//  Copyright © 2017 Daniel Illescas Romero. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
